31abe7dc8b4cd7ca4fe2d52103686174c2b57b9b
faavictoriano <victorianofrancis294@gmail.com>
2017-01-26 15:58:04 +0800

    First commit

---
+++ models/newsletter.js
@@ -0,0 +1,12 @@
+NEWSCHEMA('Newsletter').make(function(schema) {
+
+	schema.define('email', 'Email', true);
+	schema.define('ip', 'String(80)');
+
+	// Saves the model into the database
+	schema.setSave(function(error, model, options, callback) {
+		model.datecreated = F.datetime;
+		NOSQL('newsletter').insert(model);
+		callback(SUCCESS(true));
+	});
+});
